39308743f9fc99221d06c5c8f0bc8257c577e4b8
nothiaki <celsohenrique367@gmail.com>
2025-03-08 17:44:01 -0300

    ms-product:feat: record product in process

--- ms-product/src/main/java/warehouse/ms_product/app/usecase/product/ProcessProductUseCaseImpl.java
+++ ms-product/src/main/java/warehouse/ms_product/app/usecase/product/ProcessProductUseCaseImpl.java
@@ -3,8 +3,9 @@ package warehouse.ms_product.app.usecase.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//import warehouse.ms_product.app.repository.ProductRepository;
+import warehouse.ms_product.app.repository.ProductRepository;
 import warehouse.ms_product.core.domain.history.History;
+import warehouse.ms_product.core.domain.product.Product;
 import warehouse.ms_product.core.usecase.product.ProcessProductUseCase;
 import warehouse.ms_product.core.usecase.saga.SagaFailUseCase;
 import warehouse.ms_product.core.usecase.saga.SagaSuccessUseCase;
@@ -12,8 +13,8 @@ import warehouse.ms_product.core.usecase.saga.SagaSuccessUseCase;
 @Service
 public class ProcessProductUseCaseImpl implements ProcessProductUseCase {
 
-  //@Autowired
-  //private ProductRepository productRepository;
+  @Autowired
+  private ProductRepository productRepository;
 
   @Autowired
   private SagaSuccessUseCase sagaSuccessUseCase;
@@ -25,6 +26,16 @@ public class ProcessProductUseCaseImpl implements ProcessProductUseCase {
   @Override
   public void execute(History history) {
     try {
+      history.getProducts().forEach((product) -> {
+        Product existingProduct = productRepository.findByName(product.getName())
+        .orElseThrow(() -> new RuntimeException("error while iterate products in ms-product:ProcessProductUseCaseImpl"));
+
+        Long newQuantity = existingProduct.getQuantity() - product.getQuantity();
+
+        existingProduct.setQuantity(newQuantity);
+        productRepository.save(existingProduct);
+      });
+
       sagaSuccessUseCase.execute(history);
     } catch (Exception e) {
       sagaFailUseCase.execute(history);
